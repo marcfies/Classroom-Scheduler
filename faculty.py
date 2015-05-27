922837c99398014ba44fa721ddac4c9239b621e2
Jeffrey Thompson <Thompson@d-10-156-1-162.dhcp4.washington.edu>
2015-05-27 14:56:43 -0700

    Object Orienting and decoupling classes, implementation for schedule generation, implementation of course preferred faculty expertises.

---
+++ faculty.py
@@ -0,0 +1,69 @@
+import numpy as N
+
+class Faculty(object):
+    """
+    Faculty class contains all the information that makes up a faculty member.
+    Class contains init method and a display method
+    """
+    def __init__(self, name, fullTime, numClasses,expertise1, expertise2, expertise3):
+        self.name = name
+        self.fullTime = fullTime
+        self.numClasses = numClasses
+        self.expertise1 = expertise1
+        self.expertise2 = expertise2
+        self.expertise3 = expertise3
+        self.courses = []
+        
+    def display(self):
+        return 'Faculty Name: ' + self.name \
+            + ', Full Time? ' + self.fullTime \
+            + ', Faculty member can teach ' + self.numClasses  + ' class(es)'\
+            + ' with expertise in ' + self.expertise1 + ', ' \
+            + self.expertise2 + ' and ' + self.expertise3
+
+    def addCourse(self, newCourse):
+        """ Attempts to add a course to be taught for the quarter
+
+            Input:
+                newCourse = the course to be added to the faculty's schedule
+
+            Output:
+                Returns True on successful conditions to add the course,
+                        False on failure due to space limits or time clash
+        """
+
+        # check that the faculty is able to teach another class this quarter
+        if (len(self.courses)+1 <= self.numClasses):
+            # TODO: Check to confirm no two courses overlap time, and handle
+            #       appropriately if it could be scheduled better
+            self.courses.append(newCourse)
+            return True
+        else:
+            # return false on failed addition to this faculty's schedule
+            return False
+
+    def showClassSchedule(self, quarter):
+        """ Prints the faculty's name and courses teaching for the given
+            quarter
+
+            Output:
+                Currently lists the course data for any courses assigned to 
+                the faculty
+                TODO: Add visualization of this schedule to show a weekly
+                      schedule
+        """
+        # loop through all courses assigned to the faculty
+        for course in self.courses:
+            # then call their display method
+            print course.display()
+
+
+
+
+
+
+
+
+
+
+            
---
+++ quarter.py
@@ -0,0 +1,48 @@
+import numpy as N
+
+class Quarter(object):
+	"""
+	Quarter class represents a given academic quarter at UWB. The courses
+	offered during a quarter are stored as instances corresponding to one
+	instance of the course in a quarter
+	"""
+
+	def __init__(self, year, season, courses):
+		self.year = year
+		self.season = season
+		self.courses = courses
+
+	def display(self):
+		""" Displays the relevant information about this Quarter object
+
+			Output:
+				Currently displays the timing of the quarter and the 
+				number of CSS courses being offered.
+				TODO: Expand this display method to display more informative
+					  stats and a visual representation of the offered courses
+		"""
+		return str(self.year) + ' ' + self.season + " Quarter:\n" + \
+			str(len(self.courses)) + ' Courses \n'
+
+	def getPreferredExpertise(self, expertise):
+		""" Returns a list of all courses that rely on a given expertise
+
+			Input:
+				expertise = the string literal of the desired expertise
+
+			Output:
+				Returns a list that is a sublist of self.courses 
+				of all courses who meet the expected criteria,
+				currently in no particular order
+		"""
+		# initialize empty list for return value
+		matchingCourses = []
+
+		# loop through scheduled courses for those that meet the criteria
+		for courseInst in self.courses:
+			# course found when the desired expertise is present
+			if (expertise == courseInst.prefExpertise):
+				# add a reference to that course to the return list
+				matchingCourses.append(courseInst)
+		# return the list of courses, empty or not
+		return matchingCourses
